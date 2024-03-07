22822073b46ed544502118f46571423bb0a94f21
TheSquish18 <mp5896@nyu.edu>
2024-03-06 22:26:56 -0500

    Pause menu

---
+++ MSSTowerDefense2D/Assets/Scripts/UI/PauseScript.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    // use the slashed out stuff if instead want the players still in the store to keep moving (alternate universe pause)
+    //private PathProcessor.GraphUpdateLock graphLock;
+
+    //graphLock = AstarPath.active.PausePathfinding();
+    // Here we can modify the graphs safely. For example by increasing the penalty of a node
+    //AstarPath.active.data.gridGraph.GetNode(0, 0).Penalty += 1000;
+
+    // Allow pathfinding to resume
+    //graphLock.Release();
+
+    [SerializeField] GameObject popUp;
+    [SerializeField] GameObject gameManager;
+
+    public void pauseGame()
+    {
+        stopAI();
+        disableShelves();
+        enableUI();
+        Time.timeScale = 0.0f;
+    }
+
+    public void resumeGame()
+    {
+        startAI();
+        enableShelves();
+        disableUI();
+        Time.timeScale = 1.0f;
+    }
+
+    private void stopAI()
+    {
+        var ai = FindObjectsOfType<CircleCollider2D>();
+        foreach (var bot in ai)
+        {
+            bot.gameObject.GetComponent<IAstarAI>().canMove = false;
+        }
+    }
+
+    private void startAI()
+    {
+        var ai = FindObjectsOfType<CircleCollider2D>();
+        foreach (var bot in ai)
+        {
+            bot.gameObject.GetComponent<IAstarAI>().canMove = true;
+        }
+    }
+
+    private void enableUI()
+    {
+        popUp.SetActive(true);
+    }
+
+    private void disableUI()
+    {
+        popUp.SetActive(false);
+    }
+
+    private void disableShelves()
+    {
+        gameManager.GetComponent<ShelfPlacementManager>().enabled = false;
+    }
+
+    private void enableShelves()
+    {
+        gameManager.GetComponent<ShelfPlacementManager>().enabled = true;
+    }
+}
