40161ef5949376382600c0b77759648fccff945b
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2024-03-25 13:26:46 -0400

    AudioManager Created

---
+++ MSSTowerDefense2D/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public AudioSource[] audioSources;
+
+    private void Awake()
+    {
+        // Singleton pattern to ensure only one instance exists
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        // Initialize audio sources
+        audioSources = GetComponents<AudioSource>();
+    }
+
+    // Play a single audio clip
+    public void PlaySound(AudioClip clip, float volume = 1f)
+    {
+        foreach (var source in audioSources)
+        {
+            if (!source.isPlaying)
+            {
+                source.clip = clip;
+                source.volume = volume;
+                source.Play();
+                return;
+            }
+        }
+    }
+
+    // Stop all audio sources
+    public void StopAllSounds()
+    {
+        foreach (var source in audioSources)
+        {
+            source.Stop();
+        }
+    }
+
+    // Pause all audio sources
+    public void PauseAllSounds()
+    {
+        foreach (var source in audioSources)
+        {
+            source.Pause();
+        }
+    }
+
+    // Adjust the volume of all audio sources
+    public void SetVolume(float volume)
+    {
+        foreach (var source in audioSources)
+        {
+            source.volume = volume;
+        }
+    }
+}
