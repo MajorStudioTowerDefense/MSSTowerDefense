c51bcd72209ada385336ecc561ba75785357feaf
TheSquish18 <mp5896@nyu.edu>
2024-03-28 00:43:10 -0400

    New Start Screen (incomplete)

---
+++ MSSTowerDefense2D/Assets/Scripts/UI/DoorScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DoorScript : MonoBehaviour, IPointerEnterHandler
+{
+    public RectTransform rectTransform;
+    float startScaleX;
+    Vector2 startScale;
+    Vector2 endScale;
+    bool inside = false;
+
+
+    private void Start()
+    {
+        startScaleX = rectTransform.localScale.x;
+        endScale = new Vector2(0.05f, startScale.y);
+        startScale = rectTransform.localScale;
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        inside = true;
+        StartCoroutine(closingTime());
+    }
+
+    IEnumerator closingTime()
+    {
+        yield return new WaitForSeconds(1.5f);
+        inside = false;
+    }
+
+    private void Update()
+    {
+
+        if (inside == true)
+        {
+            if (rectTransform.localScale.x <= 0.1f)
+            {
+                rectTransform.localScale = endScale;
+            }
+            else
+            {
+                rectTransform.localScale = new Vector2(rectTransform.localScale.x - 0.008f, startScale.y);
+            }
+        }
+        else
+        {
+            if (rectTransform.localScale.x >= startScaleX)
+            {
+                rectTransform.localScale = startScale;
+            }
+            else
+            {
+                rectTransform.localScale = new Vector2(rectTransform.localScale.x + 0.002f, startScale.y);
+
+            }
+        }
+    }
+}
