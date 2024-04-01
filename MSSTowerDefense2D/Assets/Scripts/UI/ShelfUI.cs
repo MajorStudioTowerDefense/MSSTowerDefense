9ccc32034014e7e0b9f69ed36c72babc1ada6826
TheSquish18 <mp5896@nyu.edu>
2024-04-01 02:04:44 -0400

    Remade Shelf UI, Day Tracker almost works correctly (needs numbers)

---
+++ MSSTowerDefense2D/Assets/Scripts/UI/ShelfUI.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShelfUI : MonoBehaviour
+{
+    public GameObject FruitShelves;
+    public GameObject WeaponShelves;
+    public GameObject Other;
+
+    public GameObject FruitTab;
+    public GameObject WeaponTab;
+    public GameObject OtherTab;
+
+    private List<GameObject> tabs;
+    private List<GameObject> tabButtons;
+
+
+    private void Start()
+    {
+        tabs = new List<GameObject>() { FruitShelves, WeaponShelves, Other};
+        tabButtons = new List<GameObject>() { FruitTab, WeaponTab, OtherTab };
+        FruitShelvesOn();
+    }
+
+    private void Update()
+    {
+        if(GameManager.instance.currentState != 0)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    public void FruitShelvesOn()
+    {
+        Reset();
+        FruitShelves.SetActive(true);
+        FruitTab.GetComponent<Image>().color = Color.white;
+
+    }
+
+    public void WeaponShelvesOn()
+    {
+        Reset();
+        WeaponShelves.SetActive(true);
+        WeaponTab.GetComponent<Image>().color = Color.white;
+
+    }
+
+    public void OtherOn()
+    {
+        Reset();
+        Other.SetActive(true);
+        OtherTab.GetComponent<Image>().color = Color.white;
+
+
+    }
+
+    public void Reset()
+    {
+        foreach (GameObject tab in tabs)
+        {
+            tab.SetActive(false);
+        }
+        foreach (GameObject tabButton in tabButtons)
+        {
+            tabButton.GetComponent<Image>().color = Color.grey;
+        }
+    }
+}
