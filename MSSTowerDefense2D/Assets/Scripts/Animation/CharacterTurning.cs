6a5a4195230e96e16759f1b528c29fc3cae581e6
TheSquish18 <mp5896@nyu.edu>
2024-04-03 16:36:00 -0400

    more customers

---
+++ MSSTowerDefense2D/Assets/Scripts/Animation/CharacterTurning.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+public class CharacterTurning : MonoBehaviour
+{
+    public Sprite front;
+    public Sprite back;
+    public Sprite forward;
+    public IAstarAI path;
+    private SpriteRenderer thisRenderer;
+
+    private Vector3 left;
+    private Vector3 right;
+
+    void Start()
+    {
+        thisRenderer = GetComponent<SpriteRenderer>();
+        path = GetComponent<IAstarAI>();
+        right = new Vector3(-1, 1, 1);
+        left = Vector3.one;
+    }
+
+    void Update()
+    {
+        if (path.desiredVelocity.y > 0 && path.desiredVelocity.x > 0)
+        {
+           thisRenderer.sprite = back;
+           transform.localScale = right;
+        }
+        else if (path.desiredVelocity.y > 0 && path.desiredVelocity.x < 0)
+        {
+            thisRenderer.sprite = back;
+            transform.localScale = left;
+        }
+        else if (path.desiredVelocity.y < 0 && path.desiredVelocity.x > 0)
+        {
+            thisRenderer.sprite = forward;
+            transform.localScale = right;
+        }
+        else if (path.desiredVelocity.y < 0 && path.desiredVelocity.x < 0)
+        {
+            thisRenderer.sprite = forward;
+            transform.localScale = left;
+        }
+        else if (path.desiredVelocity.y < 0 && path.desiredVelocity.x == 0)
+        {
+            thisRenderer.sprite = front;
+            transform.localScale = left;
+        }
+    }
+}
