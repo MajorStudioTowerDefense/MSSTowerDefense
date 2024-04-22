de0aad99583d998fad97ad597061bad1af29e14d
Firemanwolf <57378965+Firemanwolf@users.noreply.github.com>
2024-04-22 11:53:39 -0400

    changes update

--- MSSTowerDefense2D/Assets/Scripts/Room.cs
+++ MSSTowerDefense2D/Assets/Scripts/Room.cs
@@ -7,8 +7,7 @@ public class Room : MonoBehaviour
     //0:U, 1:D, 2:L, 3:R
     public GameObject[] walls;
     [HideInInspector] public Vector3 roomPos;
-    public List<List<string>> roomLayout;
-    [HideInInspector] public List<Vector2> exitPos;
+public List<List<string>> roomLayout; 
 
     public void Init(Vector3 position, List<List<string>> layout)
     {
