46e6b2c03b39151033f9b37039dd5ff428af0387
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2024-04-22 12:40:27 -0400

    Customer Pissed Added!

--- MSSTowerDefense2D/Assets/Scripts/Customer Script/NormalCustomer.cs
+++ MSSTowerDefense2D/Assets/Scripts/Customer Script/NormalCustomer.cs
@@ -124,6 +124,7 @@ public class NormalCustomer : Bot
         else
         {
             targetColor = Color.Lerp(red, yellow, normalizedPatience * 2);
+            AudioManager.instance.PlaySound(CustomerPissed);
         }
 
         patienceSpriteRenderer.color = targetColor;
