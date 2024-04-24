0688815d569809472989f7be0f14d1ed626c6a63
Sttrevens <Sttrevens@126.com>
2024-04-24 13:35:32 -0400

    Update CSVReader.cs

--- MSSTowerDefense2D/Assets/Scripts/CSVReader.cs
+++ MSSTowerDefense2D/Assets/Scripts/CSVReader.cs
@@ -6,32 +6,62 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-public class CSVReader {
-	static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
-	static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
-	static char[] TRIM_CHARS = { '\"' };
+public class CSVReader
+{
+    static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
+    static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
+    static char[] TRIM_CHARS = { '\"' };
 
-	public static List<List<string>> Read(string file) {
-		var list = new List<List<string>>();
-		TextAsset data = Resources.Load(file) as TextAsset;
+    public static List<List<string>> Read(string file)
+    {
+        var list = new List<List<string>>();
+        TextAsset data = Resources.Load(file) as TextAsset;
 
-		var lines = Regex.Split(data.text, LINE_SPLIT_RE);
+        var lines = Regex.Split(data.text, LINE_SPLIT_RE);
 
-		if (lines.Length == 0) return list;
+        if (lines.Length == 0) return list;
 
-		for (var i = 0; i < lines.Length; i++) {
+        for (var i = 0; i < lines.Length; i++)
+        {
 
-			var values = Regex.Split(lines[i], SPLIT_RE);
-			if (values.Length == 0 || values[0] == "") continue;
+            var values = Regex.Split(lines[i], SPLIT_RE);
+            if (values.Length == 0 || values[0] == "") continue;
 
-			var entry = new List<string>();
-			for (var j = 0; j < values.Length; j++) {
-				string value = values[j];
-				value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
-				entry.Add(value);
-			}
-			list.Add(entry);
-		}
-		return list;
-	}
+            var entry = new List<string>();
+            for (var j = 0; j < values.Length; j++)
+            {
+                string value = values[j];
+                value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
+                entry.Add(value);
+            }
+            list.Add(entry);
+        }
+        return list;
+    }
+
+    public static List<List<string>> Read(TextAsset file)
+    {
+        var list = new List<List<string>>();
+
+        var lines = Regex.Split(file.text, LINE_SPLIT_RE);
+
+        if (lines.Length == 0) return list;
+
+        for (var i = 0; i < lines.Length; i++)
+        {
+
+            var values = Regex.Split(lines[i], SPLIT_RE);
+            if (values.Length == 0 || values[0] == "") continue;
+
+            var entry = new List<string>();
+            for (var j = 0; j < values.Length; j++)
+            {
+                string value = values[j];
+                value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
+                entry.Add(value);
+            }
+            list.Add(entry);
+        }
+        return list;
+    }
 }
